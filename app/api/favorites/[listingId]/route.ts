4180d051eec7f26d4deb7312327008c211bbcc53
dhruvsuri789 <dhruvsuri789@gmail.com>
2024-08-12 13:39:51 +0530

    Working on Listings p2

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,66 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prismaClient from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+interface IParams {
+  listingId?: string;
+}
+
+//Favorite a listing
+export async function POST(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+  favoriteIds.push(listingId);
+
+  const user = await prismaClient.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+//Un-Favorite a listing
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prismaClient.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,61 @@
+import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import useLoginModal from "./useLoginModal";
+import { useCallback, useMemo } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: User | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const favoriteIds = currentUser?.favoriteIds || [];
+    return favoriteIds.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+          /* request = await fetch(`/api/favorites/${listingId}`, {
+            method: "DELETE",
+          }); */
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
+          /* request = await fetch(`/api/favorites/${listingId}`, {
+            method: "POST",
+          }); */
+        }
+
+        await request();
+        router.refresh();
+        toast.success("Success");
+      } catch (error) {
+        toast.error("Something went wrong");
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
